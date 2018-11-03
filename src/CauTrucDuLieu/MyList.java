7a145c4417d61fe52d24b424d33932cb69f9f76f
maitansang200599@gmail.com <maitansang200599@gmail.com>
2018-11-03 23:03:30 +0700

    Exercise

---
+++ src/CauTrucDuLieu/MyList.java
@@ -0,0 +1,101 @@
+package CauTrucDuLieu;
+
+import java.util.ArrayList;
+
+public class    MyList<E> {
+    private int size =0;
+    static final int DEAFAULT_CAPACITY=10;
+    Object elements[];
+
+    public MyList( ) { }
+    public MyList(int capacity){
+        this.elements= new Object[capacity];
+    }
+    public void add(int index,E element){
+        if(index>=0 && index<=DEAFAULT_CAPACITY)
+        {
+
+            for(int i=size; i>index; i--)
+                elements[i] = elements[i-1];
+
+            size++;
+        elements[index]=element;
+
+        }
+        else
+            System.out.printf("\nVi tri %d khong hop le.",index );
+
+    }
+    E remove(int index) {
+        E element= (E)elements[index];
+        for(int i = 0; i < size; i++) {
+
+            if(i == index) {
+                size--;
+                for(int j=i; j < size; j++) {
+                    elements[j] = elements[j + 1];
+                }
+                return element;
+            }
+
+        }
+        return null;
+    }
+    public int size(){
+        return size;
+        }
+    public int indexOf(E o){
+        for (int i = 0; i <size ; i++) {
+            if (elements[i]==o){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public E get (int i){
+        for (int j = 0; j <size ; j++) {
+            if(j==i){
+                return (E)elements[j];
+            }
+        }
+            return null;
+    }
+    public Object clone (){
+        MyList<Object> myListClone = new MyList<Object>(this.elements.length-1);
+        for (int i = 0; i <myListClone.elements.length ; i++) {
+            myListClone.elements[i]=this.elements[i];
+        }
+        myListClone.size=size-1;
+        return  myListClone;
+    }
+    public void clear(){
+        for (int i = 0; i <size ; i++) {
+            elements[i]=null;
+        }
+        size=0;
+    }
+    public boolean contains(E o){
+        for (int i = 0; i <size ; i++) {
+            if(elements[i]==o){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean add(E toAdd){
+        if (toAdd!=null){
+            add(size,toAdd);
+            return true;
+        }
+        else return false;
+    }
+    public void ensureCapacity(int minCapacity){
+        if (minCapacity>size){
+            MyList<E> newMyList = new MyList<E>(minCapacity+10);
+            for (int i = 0; i <size ; i++) {
+                newMyList.elements[i]=elements[i];
+            }
+            elements =  newMyList.elements;
+        }
+    }
+}
---
+++ src/CauTrucDuLieu/MyListTest.java
@@ -0,0 +1,65 @@
+package CauTrucDuLieu;
+
+import java.util.ArrayList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<String> myList = new MyList<String>(6);
+        myList.add(0, "ádff");
+        myList.add(1, "bbbf");
+        myList.add(2, "cccf");
+        myList.add(3, "ddf");
+        myList.add(4, "eeeff");
+        myList.add(5, "33333333");
+        System.out.println("--------------list-------------");
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println("element " + i + " is " + myList.elements[i]);
+        }
+        System.out.println("------------test remove --------------");
+        System.out.println("element 2 :" + myList.remove(2) + " da bi xoa");
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println("element " + i + " is: " + myList.elements[i]);
+        }
+        System.out.println("------------ test clone  --------------");
+        MyList<String> myListClone = (MyList) myList.clone();
+        for (int i = 0; i < myListClone.elements.length; i++) {
+            System.out.println("element clone " + i + " is " + myListClone.elements[i]);
+        }
+        System.out.println("------------test indexOf --------------");
+        System.out.println("indexOf " + "ddf is " + myList.indexOf("ddf"));
+        System.out.println("------------ test get E --------------");
+        System.out.println("element 2 is " + myList.get(2));
+
+        System.out.println("------------test boolean add --------------");
+        System.out.println("after add element ");
+        System.out.println(myList.add("add"));
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println("element " + i + " is " + myList.elements[i]);
+        }
+
+        System.out.println("------------ test contains --------------");
+
+        boolean check = myList.contains("ádff");
+        if(check==true){
+            System.out.println("co phan tu ádff");
+        }else System.out.println("khong co phan tu ddf");
+        System.out.println("------------ test ensureCapacity --------------");
+        myList.ensureCapacity(10);
+        System.out.println("so phan tu la"+ myList.elements.length);
+        myList.add(5,"123");
+        myList.add(6,"222");
+        myList.add(7,"333");
+        myList.add(8,"4443");
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println("element " + i + " is " + myList.elements[i]);
+        }
+        System.out.println("------------ test clear --------------");
+        myList.clear();
+        System.out.println("so phan tu cua myList sau khi clear la " + myList.size());
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println("element " + i + " is: " + myList.elements[i]);
+        }
+
+
+    }
+}
